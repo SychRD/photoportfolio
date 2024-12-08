70f8497ed1a088cb868539a80380d4c38afe9bca
stch <yaroslavrossinsky@MacBook-Air-Yaroslav.local>
2024-12-08 17:02:47 +0300

    swagger

--- Program.cs
+++ Program.cs
@@ -1,5 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
 var app = builder.Build();
+app.UseRouting();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapGet("/", () => "Hello World!");
 
