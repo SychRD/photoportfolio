0e5a8dd71bd30f4eb9429f5d82580f4d76f54c69
stch <yaroslavrossinsky@MacBook-Air-Yaroslav.local>
2024-12-10 19:25:29 +0300

    Видимо я добавил логирование на сайт

--- Program.cs
+++ Program.cs
@@ -1,11 +1,15 @@
+using PhotoPortfolio;
 var builder = WebApplication.CreateBuilder(args);
+builder.Logging.ClearProviders();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 var app = builder.Build();
+app.UseAuthorization();
 app.UseRouting();
 app.UseSwagger();
 app.UseSwaggerUI();
-
-app.MapGet("/", () => "Hello World!");
-
+app.MapGet("/", () => "Контора Сыча");
+app.UseMiddleware<LogMiddleware>();
+app.MapControllers();
 app.Run();
+
